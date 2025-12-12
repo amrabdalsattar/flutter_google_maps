e22b8a2e59513b388a657a7b2633ed38d851e14d
amrabdalsattar <amrmmohamed00@gmail.com>
2025-12-13 00:31:16 +0200

    s

--- lib/features/google_maps/widgets/custom_google_map.dart
+++ lib/features/google_maps/widgets/custom_google_map.dart
@@ -30,7 +30,6 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   @override
   void initState() {
     super.initState();
-    
     _kGooglePlex = const CameraPosition(target: LatLng(31, 41));
     _requestLocationPermission();
   }
