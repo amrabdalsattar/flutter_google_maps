24db7ff9458a548f1a215fa2c048760d3f50a7d5
hossamgamal-2 <amrabdelsattar.dev@gmail.com>
2025-12-12 23:56:49 +0200

    - separated google maps widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,18 @@
-import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'features/google_maps/widgets/custom_google_map.dart';
 
 void main() {
   runApp(const MapSample());
 }
 
-class MapSample extends StatefulWidget {
+class MapSample extends StatelessWidget {
   const MapSample({super.key});
 
   @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  @override
-  void initState() {
-    super.initState();
-    _requestLocationPermission();
-  }
-
-  Future<void> _requestLocationPermission() async {
-    final status = await Permission.location.request();
-    if (status.isGranted) {
-      log('Location permission granted');
-    } else {
-      log('Location permission denied');
-    }
-  }
-
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: GoogleMap(
-            mapType: MapType.hybrid,
-            initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            },
-            minMaxZoomPreference: const MinMaxZoomPreference(5, 20),
-            rotateGesturesEnabled: true,
-            tiltGesturesEnabled: true,
-            zoomGesturesEnabled: true,
-            scrollGesturesEnabled: true,
-          ),
-        ),
-      ),
+      home: Scaffold(body: CustomGoogleMap()),
     );
   }
 }
